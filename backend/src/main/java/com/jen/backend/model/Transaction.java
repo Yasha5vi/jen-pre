bb86c108b143e7064168affdd74bf2791abbeb8d
Yasha5vi <yashasvichandra84@gmail.com>
2026-02-28 11:55:27 +0530

    repository setup

---
+++ backend/src/main/java/com/jen/backend/model/Transaction.java
@@ -0,0 +1,10 @@
+package com.jen.backend.model;
+
+import java.time.Instant;
+
+public class Transaction {
+    private String id;
+    private double amount;
+    private String type;
+    private Instant timestamp;
+}
